96b00930885f5c8f33d658e837a4627078d65630
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-01-13 10:26:16 +0200

    adding heading section

--- src/layout/MainNavigation.js
+++ src/layout/MainNavigation.js
@@ -5,23 +5,29 @@ import classes from "./MainNavigation.module.css";
 const MainNavigation = () => {
   return (
     <nav>
-      <div>
-        <h1>Logo</h1>
+      <div className={`card ${classes.nav__section}`}>
+        <div>
+          <img
+            className={classes.logo}
+            src={require("../assets/logo(1).png")}
+            alt="logo"
+          ></img>
+        </div>
+        <ul className={classes.nav}>
+          <li>
+            <Link to="/">Начало</Link>
+          </li>
+          <li>
+            <a href="#продукти">Продукти</a>
+          </li>
+          <li>
+            <a href="#за-нас">За нас</a>
+          </li>
+          <li>
+            <a href="#контакти">Контакти</a>
+          </li>
+        </ul>
       </div>
-      <ul className={classes.nav}>
-        <li>
-          <Link to="/">Начало</Link>
-        </li>
-        <li>
-          <a href="#продукти">Продукти</a>
-        </li>
-        <li>
-          <a href="#за-нас">За нас</a>
-        </li>
-        <li>
-          <a href="#контакти">Контакти</a>
-        </li>
-      </ul>
     </nav>
   );
 };
--- src/screens/Header.js
+++ src/screens/Header.js
@@ -3,11 +3,24 @@ import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div className={classes.bg}>
-      <div className={classes.radient__bg}>
-        <h2>We own this place</h2>
-      </div>
-    </div>
+    <section className={classes.bg}>
+      <article className={classes.radient__bg}>
+        <div className="card">
+          <div className={classes.heading__section}>
+            <h1 className={classes.store__heading}>Bodlev Store</h1>
+            <div className={classes.subheading}>
+              <h2 className={classes.subheading__text}>
+                Вашият доверен партньор при търговията на едро с бобови, зърнени
+                култури, варива и ориз.
+              </h2>
+            </div>
+            <a className={classes.button__secondary} href="#products">
+              Към продуктите ни
+            </a>
+          </div>
+        </div>
+      </article>
+    </section>
   );
 };
 
