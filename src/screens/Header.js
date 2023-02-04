39b34316d8e412b3205596d24189a1f0a345eab9
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-02-04 09:45:38 +0200

    animations with react-spring on header

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSpring, animated } from "react-spring";
 import classes from "./Header.module.css";
 
 const Header = () => {
+  const fade = useSpring({ from: { opacity: 0 }, opacity: 1 });
+  // const slideIn = useSpring({
+  //   transform: { translate3d: "-100%, 0, 0" },
+  //   translate3d: "0, 0, 0",
+  // });
+
   return (
     <section className={classes.bg}>
       <article className={classes.radient__bg}>
         <div className="card">
           <div className={classes.heading__section}>
-            <h1 className={classes.store__heading}>Bodlev Store</h1>
+            <animated.h1 style={fade} className={classes.store__heading}>
+              Bodlev Store
+            </animated.h1>
             <div className={classes.subheading}>
-              <h2 className={classes.subheading__text}>
+              <animated.h2 style={fade} className={classes.subheading__text}>
                 Вашият доверен партньор при търговията на едро с бобови, зърнени
                 култури, варива и ориз.
-              </h2>
+              </animated.h2>
             </div>
-            <a className={classes.button__secondary} href="#products">
+            <animated.a
+              style={fade}
+              className={classes.button__secondary}
+              href="#products"
+            >
               Към продуктите ни
-            </a>
+            </animated.a>
           </div>
         </div>
       </article>
--- src/screens/Products/Products.js
+++ src/screens/Products/Products.js
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 import classes from "./Products.module.css";
+import { useTransition, animated } from "react-spring";
 
 const Products = () => {
-  const [active, setActive] = useState("Ориз");
+  // const [active, setActive] = useState("Ориз");
+  const [active, setActive] = useState([
+    { type: "Ориз", key: 1 },
+    { type: "Варива", key: 2 },
+    { type: "Зърнени", key: 3 },
+  ]);
+
+  const transition = useTransition(active, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
 
   const onCategoryChange = (e) => {
     console.log(e.target.name);
-    setActive(e.target.name);
+    setActive({ type: e.target.name, key: e.target.key });
   };
 
   const categories = [
@@ -25,118 +37,110 @@ const Products = () => {
       </div>
       <div className={classes.products__section}>
         <div className={classes.categories}>
-          <li>
-            <button
-              onClick={onCategoryChange}
-              className={`${classes.single__category} ${
-                active === "Ориз" ? classes.active__category : null
-              }`}
-              name="Ориз"
-            >
-              Ориз
-            </button>
-          </li>
-
-          <li>
-            <button
-              onClick={onCategoryChange}
-              className={`${classes.single__category} ${
-                active === "Варива" ? classes.active__category : null
-              }`}
-              name="Варива"
-            >
-              Варива
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={onCategoryChange}
-              className={`${classes.single__category} ${
-                active === "Зърнени" ? classes.active__category : null
-              }`}
-              name="Зърнени"
-            >
-              Зърнени
-            </button>
-          </li>
+          <div className={classes.div__width}>
+            <li>
+              <button
+                onClick={onCategoryChange}
+                className={`${classes.single__category} ${
+                  active === "Ориз" ? classes.active__category : null
+                }`}
+                name="Ориз"
+                key={1}
+              >
+                Ориз
+              </button>
+            </li>
+          </div>
+          <div className={classes.div__width}>
+            <li>
+              <button
+                onClick={onCategoryChange}
+                className={`${classes.single__category} ${
+                  active === "Варива" ? classes.active__category : null
+                }`}
+                name="Варива"
+                key={2}
+              >
+                Варива
+              </button>
+            </li>
+          </div>
+          <div className={classes.div__width}>
+            <li>
+              <button
+                onClick={onCategoryChange}
+                className={`${classes.single__category} ${
+                  active === "Зърнени" ? classes.active__category : null
+                }`}
+                name="Зърнени"
+                key={3}
+              >
+                Зърнени
+              </button>
+            </li>
+          </div>
         </div>
-        <div>
-          <article className={classes.category__second}>
-            <div>
-              <p>
-                Оризът е основна храна за над половината от световното
-                население. На ориза се падат 20% от използваната хранителна
-                енергия в света, богат е на въглехидрати - от 75 до 85% и
-                протеини - от 5 до 10%. Тези два компонента са с много висока
-                усвояемост за организма на човека – около 96 %, поради което
-                храненето с ориз е много ефективно. Ние от Бодлев Стор
-                разполагаме с висококачествени сортове собствено производство,
-                както и такива от наши контрагенти от целия свят.
-              </p>
-              <div className={classes.btns}>
-                <button className="btn__primary">Наше производство</button>
-                <button className="btn__primary">От света</button>
+
+        {/* <div>
+          {active === "Ориз" ? (
+            <article className={classes.category__second}>
+              <div>
+                <p>
+                  Оризът е основна храна за над половината от световното
+                  население. На ориза се падат 20% от използваната хранителна
+                  енергия в света, богат е на въглехидрати - от 75 до 85% и
+                  протеини - от 5 до 10%. Тези два компонента са с много висока
+                  усвояемост за организма на човека – около 96 %, поради което
+                  храненето с ориз е много ефективно. Ние от Бодлев Стор
+                  разполагаме с висококачествени сортове собствено производство,
+                  както и такива от наши контрагенти от целия свят.
+                </p>
+                <div className={classes.btns}>
+                  <button className="btn__primary">Наше производство</button>
+                  <button className="btn__primary">От света</button>
+                </div>
               </div>
-            </div>
-          </article>
-        </div>
+            </article>
+          ) : active === "Варива" ? (
+            <article className={classes.category__second}>
+              <div>
+                <h2>Варива</h2>
+                <p>
+                  Варивата са част от семейството на зърнените храни, известни
+                  като бобови растения, и включват храни като нахут, грах, леща,
+                  боб и фъстъци. Те са важен хранителен източник в целия свят.
+                  Варивата са добър източник на протеини, фибри и основни
+                  хранителни вещества, което ги прави здравословно допълнение
+                  към вашата диета.
+                </p>
+                <button className="btn__primary btn__primary_left">
+                  Към продуктите
+                </button>
+              </div>
+              <img src={require("../../assets/beans1.jpg")} alt="ориз"></img>
+            </article>
+          ) : active === "Зърнени" ? (
+            <article className={classes.category__second}>
+              <div>
+                <p>
+                  Оризът е основна храна за над половината от световното
+                  население. На ориза се падат 20% от използваната хранителна
+                  енергия в света, богат е на въглехидрати - от 75 до 85% и
+                  протеини - от 5 до 10%. Тези два компонента са с много висока
+                  усвояемост за организма на човека – около 96 %, поради което
+                  храненето с ориз е много ефективно. Ние от Бодлев Стор
+                  разполагаме с висококачествени сортове собствено производство,
+                  както и такива от наши контрагенти от целия свят.
+                </p>
+                <div className={classes.btns}>
+                  <button className="btn__primary">Наше производство</button>
+                  <button className="btn__primary">От света</button>
+                </div>
+              </div>
+            </article>
+          ) : null}
+        </div> */}
       </div>
-
-      {/* <div className={classes.products__line}>
-        <article className={classes.category}>
-          <img src={require("../../assets/rice2.jpg")} alt="ориз"></img>
-          <div>
-            <h2>Ориз</h2>
-            <p>
-              Оризът е основна храна за над половината от световното население.
-              На ориза се падат 20% от използваната хранителна енергия в света,
-              богат е на въглехидрати - от 75 до 85% и протеини - от 5 до 10%.
-              Тези два компонента са с много висока усвояемост за организма на
-              човека – около 96 %, поради което храненето с ориз е много
-              ефективно. Ние от Бодлев Стор разполагаме с висококачествени
-              сортове собствено производство, както и такива от наши контрагенти
-              от целия свят.
-            </p>
-            <div className={classes.btns}>
-              <button className="btn__primary">Наше производство</button>
-              <button className="btn__primary">От света</button>
-            </div>
-          </div>
-        </article>
-        <article className={classes.category__second}>
-          <div>
-            <h2>Варива</h2>
-            <p>
-              Варивата са част от семейството на зърнените храни, известни като
-              бобови растения, и включват храни като нахут, грах, леща, боб и
-              фъстъци. Те са важен хранителен източник в целия свят. Варивата са
-              добър източник на протеини, фибри и основни хранителни вещества,
-              което ги прави здравословно допълнение към вашата диета.
-            </p>
-            <button className="btn__primary btn__primary_left">
-              Към продуктите
-            </button>
-          </div>
-          <img src={require("../../assets/beans1.jpg")} alt="ориз"></img>
-        </article>
-        <article className={classes.category}>
-          <img src={require("../../assets/rice.jpg")} alt="ориз"></img>
-          <div>
-            <h2>Други продукти</h2>
-            <p>
-              Оризът е основна храна за над половината от световното население.
-              На ориза се падат 20% от използваната хранителна енергия в света,
-              богат е на въглехидрати - от 75 до 85% и протеини - от 5 до 10%.
-              Тези два компонента са с много висока усвояемост за организма на
-              човека – около 96 %, поради което храненето с ориз е много
-              ефективно. Ние от Бодлев Стор разполагаме с висококачествени
-              сортове собствено производство, както и такива от наши контрагенти
-              от целия свят.
-            </p>
-            <button className="btn__primary">Към продуктите</button>
-          </div>
-        </article>
-      </div> */}
     </section>
   );
 };
