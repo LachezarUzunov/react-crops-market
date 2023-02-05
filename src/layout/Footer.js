82b9b11db3a8f1216deba4ed89b92d314d9cc150
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-02-05 17:12:54 +0200

    adding footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,7 +4,22 @@ import classes from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer>
-      <h4>Footer</h4>
+      <h1 className={classes.footer__heading}>BODLEV STORE</h1>
+
+      <ul className={classes.permalinks}>
+        <li>
+          <a href="#home">Начало</a>
+        </li>
+        <li>
+          <a href="#about">Продукти</a>
+        </li>
+        <li>
+          <a href="#react">За нас</a>
+        </li>
+        <li>
+          <a href="#wordpress">Контакти</a>
+        </li>
+      </ul>
     </footer>
   );
 };
